ad0646e0c3576e1673c70f61efd6b76cb7a67a34
Franco Benettini <franbenettini@gmail.com>
2022-04-01 01:10:23 -0300

    add item

---
+++ src/asyncmock.js
@@ -0,0 +1,39 @@
+const products = [
+    {
+        id:1,
+        name:'Iphone 13 Pro',
+        price:999,
+        category:'celular',
+        img:'https://www.apple.com/v/iphone/home/be/images/overview/compare/compare_iphone_13_pro__bpn3x8hs692a_large.jpg',
+        stock:25,
+        description:'Descripcion de Iphone 13 Pro'
+    },
+    {
+        id:2,
+        name:'Iphone 13',
+        price:699,
+        category:'celular',
+        img:'https://www.apple.com/v/iphone/home/be/images/overview/compare/compare_iphone_13__fqzwhmfmroey_large.jpg',
+        stock:50,
+        description:'Descripcion de Iphone 13'
+    },
+    {
+        id:3,
+        name:'Iphone SE',
+        price:429,
+        category:'celular',
+        img:'https://www.apple.com/v/iphone/home/be/images/overview/compare/compare_iphone_se__d5blqx1pgymq_large.jpg',
+        stock:30,
+        description:'Descripcion de Iphone SE'
+    }
+]
+
+
+
+export const getProducts = () => {
+    return new Promise (resolve => {
+        setTimeout (() => {
+            resolve (products)
+        }, 2000 )
+    })
+}
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
 import Counter from '../ItemCount/ItemCount';
+import ItemList from '../ItemList/ItemList';
+import { getProducts } from '../../asyncmock'
 
 const ItemListContainer = (props) => {
 
+    const [products, setProducts] = useState([])
+
+    useEffect (() => {
+        getProducts().then(prods => {
+            setProducts(prods)
+        }).catch( error => {
+            console.log(error)
+        })
+    }, [])
+
     const handleOnAdd = (quantity) => { 
          console.log(`se agregaron ${quantity} productos`)
-        
     }
 
     return(
@@ -12,6 +24,7 @@ const ItemListContainer = (props) => {
             <h2>{props.greeting}</h2>
             <Counter initial={1} stock={10} onAdd={handleOnAdd}/>
             <Counter initial={1} stock={15} onAdd={handleOnAdd}/>
+            <ItemList products = {products}/>
         </>
     )
 }
